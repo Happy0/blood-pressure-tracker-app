7087f644d6c10d9cfbdcabeb5e029f3a7f90af38
Gordon Martin <gordonhughmartin@gmail.com>
2025-12-10 22:31:28 +0000

    start fannying aboot wae a component library.

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.component('Button', Button)
 
 app.mount('#app')
