6040cce7321def62931562de6eea44ab5ed330f3
Gordon Martin <gordonhughmartin@gmail.com>
2025-12-12 23:58:10 +0000

    wip

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,18 +10,32 @@ const route = useRoute()
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <div>
-      <RouterLink v-if="route.path === '/'" to="/reading" > Take Reading </RouterLink>
-    <main>
-      <RouterView></RouterView>
-    </main>
+  <div v-if="route.path === '/'" to="/reading" class="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
+    <div class="bg-white shadow-lg rounded-2xl p-8 w-full max-w-sm flex flex-col gap-4">
+
+      <h1 class="text-2xl font-semibold text-center mb-2">
+        Main Menu
+      </h1>
+
+      <RouterLink
+        to="/reading"
+        class="text-center py-3 rounded-xl border bg-blue-500 text-white font-medium hover:bg-blue-600 transition"
+      >
+        Take Reading
+      </RouterLink>
+
+      <button
+        disabled
+        class="py-3 rounded-xl border border-gray-300 text-gray-400 bg-gray-100 cursor-not-allowed"
+      >
+        View Readings (coming soon)
+      </button>
+
+    </div>
   </div>
-  
+  <main>
+    <RouterView></RouterView>
+  </main>
 </template>
 
 <style scoped>
